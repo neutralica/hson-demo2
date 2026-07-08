bebc0e6edd0931921473145f51ef3319443f9ee3
Phillip Hanson <hansonpw@gmail.com>
2026-07-07 22:19:07 -0500

    Revert "w/.: fun w imports"
    This reverts commit 7ed33422471b84fefcc5d4dda816e271f46804ef.

--- src/tests/livemap-tests/schema-suite-2.ts
+++ src/tests/livemap-tests/schema-suite-2.ts
@@ -3,12 +3,14 @@
 import { hson } from "hson-live";
 import {
   render_livemap_schema_controls_snap,
+  type LiveControlViewBridgeTarget,
+  type LiveInputListenerResult,
+  type LiveMapSchemaControlSpec,
 } from "../../../../hson-live/src/api/livemap/bridge";
 import type { JsonValue } from "../../../../hson-live/src/core/types";
 import type { LiveMap } from "../../../../hson-live/src/api/livemap/livemap.types";
 import type { TestCase, TestSuite } from "../../app/demos/test/tests.types";
 import { equal_row, preview_value } from "./test-helpers";
-import type { LiveControlViewBridgeTarget, LiveInputListenerResult, LiveMapSchemaControlSpec } from "../../../../hson-live/src/api/livemap/bridge.types";
 
 type BridgeMap = LiveMap;
 type LiveTreeControlViewTarget = LiveControlViewBridgeTarget &
